738f1c1e60474c5d69852c553b0b7a9d5e587dde
JolanMoreau <102695705+JolanMoreau@users.noreply.github.com>
2022-03-30 21:55:29 +0200

    Create divers.h

---
+++ divers.h
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <fstream>
+#include <string.h>
+#include <cstdlib>
+
+int div_euclid(int a, int d);
+int exec_cmd(std::string cmd);
+int go_home();
+int balayage ();
+void lire_bus(int p0,int p1, int p2);
